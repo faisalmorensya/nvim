b2140c95bf822006b9422248dfe2d867b1f5dbbf
faisal.morensya <faisal.morensya@woven.toyota>
2026-02-21 00:06:18 +0900

    fix lazy git

--- lua/morensya/plugins/lazygit.lua
+++ lua/morensya/plugins/lazygit.lua
@@ -7,13 +7,17 @@ return {
     "LazyGitFilter",
     "LazyGitFilterCurrentFile",
   },
-  -- optional for floating window border decoration
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
   keys = {
     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
   },
+  config = function()
+    vim.g.lazygit_floating_window_winblend = 0 -- transparency
+    vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+    vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" } -- customize lazygit popup window border characters
+    vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+    vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+  end,
 }
